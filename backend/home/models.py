975a0194fd0afa891393997cf0c4038da6dd98d3
crowdbotics-apps <team@crowdbotics.com>
2023-02-20 16:28:31 +0000

    Update models

--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,7 @@
+from django.conf import settings
 from django.db import models
+class Pet(models.Model):
+    'Generated Model'
+    name = models.CharField(max_length=50,)
+    type = models.CharField(max_length=50,)
+    price = models.IntegerField()
